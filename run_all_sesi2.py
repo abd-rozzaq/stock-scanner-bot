d61c7db7ce7baad1951456ceaced96af5eab08f5
rozzaq <64617660+abd-rozzaq@users.noreply.github.com>
2026-05-09 14:08:34 +0000

    Duplikasi run_all.py menjadi run_all_sesi2.py

---
+++ run_all_sesi2.py
@@ -0,0 +1,17 @@
+import subprocess
+
+files = [
+    "scanner_gemini.py",
+    "screener_v4_dual.py",
+    "scanner_bsjp_v3_dual.py --mode midday",
+    "scanner_bsjp_v3_dualsession.py --mode session1",
+    "scanner_dual_session_v5.py session1",
+    "scanner_bsjp_v4_dual_session.py --session session1",
+    "grok_screener_v2.2.py --session siang"
+]
+
+processes = [subprocess.Popen(["python", f]) for f in files]
+
+# Menunggu semua proses selesai
+for p in processes:
+    p.wait()
